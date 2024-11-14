b6452b525928defe357c707d3a5ac040868e9cbb
rangssu <wlqkrtls6042@gmail.com>
2024-11-15 01:24:17 +0900

    글작성됨

--- gesuipan/src/main/java/com/example/Gesuipan/DTO/panDTO.java
+++ gesuipan/src/main/java/com/example/Gesuipan/DTO/panDTO.java
@@ -5,12 +5,44 @@ import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class panDTO {
-	private Long id;
+	private int id;
 	private String title;
 	private String content;
 	private String writer;
 	private Timestamp reg;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Timestamp getReg() {
+		return reg;
+	}
+	public void setReg(Timestamp reg) {
+		this.reg = reg;
+	}
+	
+	
+	
 }
--- gesuipan/src/main/java/com/example/Gesuipan/mapper/panMapper.java
+++ gesuipan/src/main/java/com/example/Gesuipan/mapper/panMapper.java
@@ -1,13 +1,17 @@
 package com.example.Gesuipan.mapper;
 
 import com.example.Gesuipan.DTO.panDTO;
+
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
 public interface panMapper {
-	panDTO getPan();
+	List<panDTO> getAllPan();  // 게시물 ID로 조회
+	panDTO getPanById(int id);
 	void insertpan(panDTO post);
 	void updatepan(panDTO post);
 	void deletepan(int id);
